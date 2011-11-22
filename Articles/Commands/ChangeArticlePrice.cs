40040d899a7e3e6a55ba46f80dd68f746784537e
Christian Deger <christian@deger.eu>
2011-11-22 21:23:56 +0100

    Price change commands and events created

---
+++ Articles/Commands/ChangeArticlePrice.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Infrastructure;
+
+namespace Articles.Commands
+{
+    public class ChangeArticlePrice : Command
+    {
+        public readonly Guid Id;
+        public readonly int Price;
+        public readonly int OriginalVersion;
+
+        public ChangeArticlePrice(Guid id, int price, int originalVersion)
+        {
+            Id = id;
+            Price = price;
+            OriginalVersion = originalVersion;
+        }
+    }
+}
---
+++ Articles/Events/ArticlePriceChanged.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Infrastructure;
+
+namespace Articles.Events
+{
+    public class ArticlePriceChanged : Event
+    {
+        public readonly Guid Id;
+        public readonly int Price;
+
+        public ArticlePriceChanged(Guid id, int price)
+        {
+            Id = id;
+            Price = price;
+        }
+    }
+}
