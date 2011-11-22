a5d6eb4ff89fb6e2ada3729e5adb5e22b661b1c9
Christian Deger <christian@deger.eu>
2011-11-22 21:19:06 +0100

    Rename also affects list read model

--- Articles/ReadModel/ArticleListEventHandler.cs
+++ Articles/ReadModel/ArticleListEventHandler.cs
@@ -1,13 +1,19 @@
-﻿using Articles.Events;
+﻿using System.Linq;
+using Articles.Events;
 using Infrastructure;
 
 namespace Articles.ReadModel
 {
-    public class ArticleListEventHandler : IHandles<ArticleInserted>
+    public class ArticleListEventHandler : IHandles<ArticleInserted>, IHandles<ArticleRenamed>
     {
         public void Handle(ArticleInserted message)
         {
             Database.ArticleList.Add(new ArticleListDto(message.Id, message.Name));
+        }
+
+        public void Handle(ArticleRenamed message)
+        {
+            Database.ArticleList.First(x => x.ArticleId == message.Id).Name = message.Name;
         }
     }
 }
