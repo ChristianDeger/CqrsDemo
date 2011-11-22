d7bda66b99694ff94b0334e4e2ca6c2bf1a34401
Christian Deger <christian@deger.eu>
2011-11-22 21:15:57 +0100

    Created rename article command and event

---
+++ Articles/Commands/RenameArticle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Infrastructure;
+
+namespace Articles.Commands
+{
+    public class RenameArticle : Command
+    {
+        public readonly Guid Id;
+        public readonly string Name;
+        public readonly int OriginalVersion;
+
+        public RenameArticle(Guid id, string name, int originalVersion)
+        {
+            Id = id;
+            Name = name;
+            OriginalVersion = originalVersion;
+        }
+    }
+}
---
+++ Articles/Events/ArticleRenamed.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Infrastructure;
+
+namespace Articles.Events
+{
+    public class ArticleRenamed : Event
+    {
+        public readonly Guid Id;
+        public readonly string Name;
+
+        public ArticleRenamed(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
