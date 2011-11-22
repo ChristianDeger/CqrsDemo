3a0c4792b7ceda38d9f9fc5cd585d3a7c89860a1
Christian Deger <christian@deger.eu>
2011-11-22 21:11:16 +0100

    Created article insert command and event

---
+++ Articles/Commands/InsertArticle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Infrastructure;
+
+namespace Articles.Commands
+{
+    public class InsertArticle : Command
+    {
+        public readonly Guid Id;
+        public readonly string Name;
+
+        public InsertArticle(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ Articles/Events/ArticleInserted.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Infrastructure;
+
+namespace Articles.Events
+{
+    public class ArticleInserted : Event
+    {
+        public readonly Guid Id;
+        public readonly string Name;
+
+        public ArticleInserted(Guid id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
