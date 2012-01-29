6eb5a00fdba90e234a9ecc2dca779c603245ba50
Christian Deger <christian@deger.eu>
2012-01-29 14:26:33 +0100

    Started with backbone app

---
+++ ArticlesWeb/js/articles.js
@@ -0,0 +1,24 @@
+﻿$(function() {
+    var Article, ArticleCollection, ArticlesView;
+
+    Article = Backbone.Model.extend({
+        initialize: function() {
+            console.log('this model has been initialized');
+        }
+    });
+    ArticleCollection = Backbone.Collection.extend({ model: Article });
+    ArticlesView = Backbone.View.extend({
+        el: $('#articles'),
+        render: function(event) {
+            var compiledTemplate = _.template($("#article-template").html());
+            this.el.html(compiledTemplate(this.model.toJSON()));
+            return this; //recommended as this enables calls to be chained.
+        }
+    });
+
+    var Articles = new ArticleCollection();
+    Articles.url = 'http://articlesservice/articles';
+
+    Articles.fetch();
+});
+
